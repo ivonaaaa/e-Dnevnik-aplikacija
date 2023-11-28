15fbde91879bea03927894a58d478915b46a5e62
nduran11 <nduran@pmfst.hr>
2023-11-28 15:38:02 +0100

    dodan je prikaz ispita

--- script.js
+++ script.js
@@ -45,3 +45,64 @@ function biljeske() {
 function predmet() {
     window.location.href = 'predmet.html';
 }
+
+const exams = [
+    { name: 'Ispit 1', date: new Date('2023-10-10') },
+    { name: 'Ispit 2', date: new Date('2023-11-20') },
+    { name: 'Ispit 3', date: new Date('2023-12-05') },
+    { name: 'Ispit 4', date: new Date('2023-12-15') },
+    { name: 'Ispit 5', date: new Date('2024-01-10') },
+];
+
+function showExams(period) {
+    const selectedMonth = document.getElementById('months').value;
+    const currentDate = new Date();
+
+    let filteredExams;
+    if (period === 'past') {
+        filteredExams = exams.filter(exam => exam.date < currentDate);
+    } else if (period === 'current') {
+        filteredExams = exams.filter(exam => {
+            const examMonth = exam.date.getMonth() + 1; // Mjeseci su 0-indeksirani, stoga dodajemo 1
+            const currentMonth = currentDate.getMonth() + 1;
+            return exam.date.getFullYear() === currentDate.getFullYear() && examMonth === currentMonth;
+        });
+    } else if (period === 'future') {
+        filteredExams = exams.filter(exam => exam.date > currentDate);
+    } else {
+        // Filtriraj prema odabranom mjesecu
+        filteredExams = exams.filter(exam => {
+            const examMonth = exam.date.getMonth() + 1;
+            return examMonth === parseInt(selectedMonth, 10);
+        });
+    }
+
+    displayExams(filteredExams);
+}
+
+// Prikazi tekuće ispite kao zadani pri pokretanju stranice
+window.onload = function() {
+    showExams('current');
+};
+
+function displayExams(exams) {
+    const examsContainer = document.getElementById('exams-container');
+    examsContainer.innerHTML = '';
+
+    if (exams.length === 0) {
+        examsContainer.innerHTML = '<p>Nema dostupnih ispita u odabranom razdoblju.</p>';
+        return;
+    }
+
+    const ul = document.createElement('ul');
+    exams.forEach(exam => {
+        const li = document.createElement('li');
+        li.textContent = `${exam.name} - ${exam.date.toDateString()}`;
+        ul.appendChild(li);
+    });
+
+    examsContainer.appendChild(ul);
+}
+
+// Prikazi tekuće ispite kao zadane pri pokretanju
+showExams('current');
